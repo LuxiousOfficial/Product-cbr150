0b34bad6137ab0cc2f905e8aa7397519c708d187
rommy <rommyardiansyah09@gmail.com>
2024-08-12 13:59:04 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Owl Carousel Card slider
+$('.owl-carousel').owlCarousel({
+        loop:false,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:3
+            },
+            900:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
+// End Owl Carousel Card Slider
